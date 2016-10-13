2b6eb811309719e1fcfbff1cdfc0f08a228b156a
Li Chunlin <lcl@haoju.cn>
2016-10-13 10:26:29 +0800

    文件找回

--- app/src/main/java/cn/wtkj/charge_inspect/data/rest/BusinessInfoDataImpl.java
+++ app/src/main/java/cn/wtkj/charge_inspect/data/rest/BusinessInfoDataImpl.java
@@ -1,21 +1,27 @@
 package cn.wtkj.charge_inspect.data.rest;
 
 import cn.wtkj.charge_inspect.data.SeApiManager;
+import cn.wtkj.charge_inspect.data.bean.ArticleDetail;
 import cn.wtkj.charge_inspect.data.net.DangerousApi;
+import cn.wtkj.charge_inspect.data.bean.ArticleListData;
 import retrofit.Callback;
 
 /**
  * Created by lcl on 2016/9/30.
  */
-public class BusinessInfoDataImpl implements BusinessInfoData {
+public class BusinessInfoDataImpl {
+
     private final DangerousApi dangerousApi;
 
     public BusinessInfoDataImpl() {
         dangerousApi = SeApiManager.apiMangerAdapter();
     }
 
-    @Override
-    public void getBusinessData(Callback<cn.wtkj.charge_inspect.data.bean.BusinessInfoData> callback) {
+    public void getBusinessData(Callback<ArticleListData> callback) {
         dangerousApi.getBusinessData(callback);
+    }
+
+    public void getBusinessDetail(String articleId,Callback<ArticleDetail> callback) {
+        dangerousApi.getBusinessDetail(articleId,callback);
     }
 }
